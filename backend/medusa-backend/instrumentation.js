9955b4cef35e9b5b18e48f2c0863ccd929a7a68c
w3bsuki <twstlxyz@gmail.com>
2025-06-24 19:40:20 -0700

    Fix instrumentation module not found error

---
+++ backend/medusa-backend/instrumentation.js
@@ -0,0 +1,5 @@
+// Empty instrumentation file for production
+// Uncomment this file to enable instrumentation and observability using OpenTelemetry
+// Refer to the docs for installation instructions: https://docs.medusajs.com/learn/debugging-and-testing/instrumentation
+
+module.exports = {};
